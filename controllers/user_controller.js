9c219188b3dcd42458ae05a52c54366a22401cde
mohdshehwaz <mohdshehwaz@gmail.com>
2023-01-16 22:07:28 +0530

    Add authentication in auth branch

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,9 +1,20 @@
 const User = require('../models/user');
 
 module.exports.profile = (req,res) => {
-    return res.render('user_profile',{
-        title:"Home is "
-    });
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id,(err,user) =>{
+            if(user){
+                return res.render('user_profile',{
+                    title:'user profile',
+                    user:user
+                })
+            }
+        });
+    }
+    else{
+        return res.redirect('/users/sign-in');
+    }
+    
 };
 // render the signup page
 module.exports.signup = (req,res) =>{
@@ -53,4 +64,34 @@ module.exports.create = (req,res) => {
 
 module.exports.createSession = (req,res) =>{
 
+    // steps to authenticate
+
+    // find the user
+    User.findOne({email:req.body.email},(err,user) => {
+        if(err){
+            console.log("error in creating user while signing up");
+            return;   
+        }
+        //handle user found
+        if(user){
+            if(user.password != req.body.password){
+                return res.redirect('back');
+            }
+            res.cookie('user_id',user.id);
+            return res.redirect('/users/profile');
+        }
+        else{
+            return res.redirect('back');
+        }
+        // handle mismatching password which don't match
+    
+
+        // handle session creation
+
+    })
+    
+
+    
+
+    // handle user not found
 }
